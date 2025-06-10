bc9585091b7f9248793c6da9c957828a585b9f3d
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2025-06-09 21:26:24 -0300

    Agrego saludo

--- archivo.php
+++ archivo.php
@@ -7,5 +7,20 @@
 </head>
 <body>
     <h1>Hola</h1>
+
+    <p>Este es un ejemplo de un archivo PHP que se incluye en otro archivo.</p>
+    <p>El contenido de este archivo se mostrará en la página principal.</p>
+    <p>¡Espero que te sea útil!</p>
+    <p>Recuerda que puedes modificar este contenido según tus necesidades.</p>
+    <p>¡Gracias por visitar!</p>
+    <p>¡Hasta luego!</p>
+    <p>¡Adiós!</p>
+    <p>¡Nos vemos pronto!</p>
+    <p>¡Cuídate!</p>
+    <p>¡Saludos!</p>
+    <p>¡Disfruta de tu día!</p>
+    <p>¡Sigue aprendiendo!</p>
+    <p>¡Nunca dejes de explorar!</p>
+    <p>¡La curiosidad es el motor del conocimiento!</p>
 </body>
 </html>
